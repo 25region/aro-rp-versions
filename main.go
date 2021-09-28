8b05db0622972c0cc45cfd94c46c041c6ff62817
Peter Kostyukov <pkostyuk@redhat.com>
2021-09-28 11:38:56 -0500

    Configure flags

--- main.go
+++ main.go
@@ -1,53 +1,69 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"sort"
 	"sync"
 
 	"github.com/lensesio/tableprinter"
+	"github.com/sirupsen/logrus"
+	flag "github.com/spf13/pflag"
 )
+
+var log = logrus.New()
+
+type flags struct {
+	location []string
+	output   string
+	debug    bool
+}
 
 type OCPVersion struct {
 	Version string `json:"version"`
 }
 
-type Version struct {
-	Location   string       `header:"Location"`
-	RPVersion  string       `header:"RPVersion"`
-	OCPVersion []OCPVersion `header:"OCPVersions"`
+type Result struct {
+	Location    string   `header:"Location" json:"location"`
+	RPVersion   string   `header:"RPVersion" json:"rpVersion"`
+	OCPVersions []string `header:"OCPVersions" json:"ocpVersions"`
 }
 
-func getOCPVersions(location string) ([]OCPVersion, error) {
+func getOCPVersions(location string) ([]string, error) {
 
 	url := "https://arorpversion.blob.core.windows.net/ocpversions/" + location
 
 	res, err := http.Get(url)
 
 	if err != nil || res.StatusCode != http.StatusOK {
-		log.Printf("failed to pull ocp versions: %s", err)
+		log.Debugf("failed to pull ocp versions for %q: %s", location, err)
 		return nil, err
 	}
 	defer res.Body.Close()
 
 	jsonData, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Printf("failed to read requests body: %s", err)
+		log.Debugf("failed to read requests body for %q: %s", location, err)
 		return nil, err
 	}
 
 	var ocpVersions []OCPVersion
 	err = json.Unmarshal(jsonData, &ocpVersions)
 	if err != nil {
-		log.Printf("failed to unmarshal ocp versions: %s", err)
+		log.Debugf("failed to unmarshal ocp versions for %q: %s", location, err)
 		return nil, err
 	}
 
-	return ocpVersions, nil
+	var result []string
+	for _, version := range ocpVersions {
+		result = append(result, fmt.Sprint(version.Version))
+	}
+
+	return result, nil
 }
 
 func getRPVersions(location string) (string, error) {
@@ -57,52 +73,76 @@ func getRPVersions(location string) (string, error) {
 	res, err := http.Get(url)
 
 	if err != nil || res.StatusCode != http.StatusOK {
-		log.Printf("failed to pull rp version: %s", err)
+		log.Debugf("failed to pull rp version for %q: %s", location, err)
 		return "", err
 	}
 	defer res.Body.Close()
 
 	commitVersion, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Printf("failed to read requests body: %s", err)
+		log.Debugf("failed to read requests body for %q: %s", location, err)
 		return "", err
 	}
 
 	return string(commitVersion), nil
 }
 
-func processLocation(location string, ch chan Version) {
+func processLocation(location string, ch chan Result) {
 
 	ocpVersions, err := getOCPVersions(location)
 	if err != nil {
-		log.Printf("failed to get OCP versions for %q location: %s", location, err)
+		log.Debugf("failed to get OCP versions for %q location: %s", location, err)
 	}
 
 	rpVersion, err := getRPVersions(location)
 	if err != nil {
-		log.Printf("failed to get RP version for %q location: %s", location, err)
+		log.Debugf("failed to get RP version for %q location: %s", location, err)
 	}
 
-	locationResult := Version{
-		Location:   location,
-		RPVersion:  rpVersion,
-		OCPVersion: ocpVersions,
+	locationResult := Result{
+		Location:    location,
+		RPVersion:   rpVersion,
+		OCPVersions: ocpVersions,
 	}
 
 	ch <- locationResult
 }
 
+func parseFlags() flags {
+
+	var f flags
+	flag.StringSliceVarP(&f.location, "location", "l", []string{}, "comma-separated Azure regions")
+	flag.StringVarP(&f.output, "output", "o", "table", "defines output format (table|json)")
+	flag.BoolVarP(&f.debug, "debug", "d", false, "enable debugging output")
+	flag.Parse()
+
+	return f
+}
+
 func main() {
 
-	// TODO: Pull dynamically eventually
-	locations := []string{
-		"eastus2euap", "westcentralus", "australiaeast", "japaneast", "koreacentral",
-		"australiasoutheast", "centralindia", "southindia", "japanwest", "eastasia",
-		"centralus", "eastus", "eastus2", "northcentralus", "southcentralus",
-		"westus", "westus2", "canadacentral", "canadaeast", "francecentral",
-		"germanywestcentral", "northeurope", "norwayeast", "switzerlandnorth", "switzerlandwest",
-		"westeurope", "brazilsouth", "brazilsoutheast", "southeastasia", "uaenorth",
-		"southafricanorth", "uksouth", "ukwest",
+	flags := parseFlags()
+
+	// Configure logging level
+	log.SetOutput(os.Stdout)
+	if flags.debug {
+		log.Level = logrus.DebugLevel
+	}
+
+	var locations []string
+	if len(flags.location) > 0 {
+		locations = flags.location
+	} else {
+		// TODO: Pull dynamically eventually
+		locations = []string{
+			"eastus2euap", "westcentralus", "australiaeast", "japaneast", "koreacentral",
+			"australiasoutheast", "centralindia", "southindia", "japanwest", "eastasia",
+			"centralus", "eastus", "eastus2", "northcentralus", "southcentralus",
+			"westus", "westus2", "canadacentral", "canadaeast", "francecentral",
+			"germanywestcentral", "northeurope", "norwayeast", "switzerlandnorth", "switzerlandwest",
+			"westeurope", "brazilsouth", "brazilsoutheast", "southeastasia", "uaenorth",
+			"southafricanorth", "uksouth", "ukwest",
+		}
 	}
 
 	locationsCount := len(locations)
@@ -110,7 +150,7 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(locationsCount)
 
-	ch := make(chan Version, locationsCount)
+	ch := make(chan Result, locationsCount)
 
 	for _, location := range locations {
 
@@ -125,7 +165,7 @@ func main() {
 	wg.Wait()
 	close(ch)
 
-	var results []Version
+	var results []Result
 
 	for result := range ch {
 		results = append(results, result)
@@ -135,17 +175,34 @@ func main() {
 		return results[j].Location > results[i].Location
 	})
 
-	printer := tableprinter.New(os.Stdout)
+	switch o := flags.output; o {
+	case "json":
 
-	// Optionally, customize the table, import of the underline 'tablewriter' package is required for that.
-	// printer.BorderTop, printer.BorderBottom, printer.BorderLeft, printer.BorderRight = true, true, true, true
-	// printer.CenterSeparator = "│"
-	// printer.ColumnSeparator = "│"
-	// printer.RowSeparator = "─"
-	// printer.HeaderBgColor = tablewriter.BgBlackColor
-	// printer.HeaderFgColor = tablewriter.FgGreenColor
+		var err error
+		in, err := json.Marshal(results)
+		if err != nil {
+			log.Fatalf("failed to marshal the result in json format: %s", err)
+		}
+		var out bytes.Buffer
+		err = json.Indent(&out, []byte(in), "", "  ")
+		if err != nil {
+			log.Fatalf("failed to output the result in json format: %s", err)
+		}
+		fmt.Println(&out)
 
-	// Print the slice of structs as table, as shown above.
-	printer.Print(results)
+	default:
+		printer := tableprinter.New(os.Stdout)
+
+		// Optionally, customize the table, import of the underline 'tablewriter' package is required for that.
+		// printer.BorderTop, printer.BorderBottom, printer.BorderLeft, printer.BorderRight = true, true, true, true
+		// printer.CenterSeparator = "│"
+		// printer.ColumnSeparator = "│"
+		// printer.RowSeparator = "─"
+		// printer.HeaderBgColor = tablewriter.BgBlackColor
+		// printer.HeaderFgColor = tablewriter.FgGreenColor
+
+		// Print the slice of structs as table, as shown above.
+		printer.Print(results)
+	}
 
 }
